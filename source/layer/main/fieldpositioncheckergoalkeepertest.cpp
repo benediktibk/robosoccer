90770c6952f68b3c604da5fd29be057100f8bb10
Kevin Meyer <meyer.kevin@mytum.de>
2014-04-26 13:31:03 +0200

    added tests for goalkeeperPositionChecker

--- source/layer/main/fieldpositioncheckergoalkeepertest.cpp
+++ source/layer/main/fieldpositioncheckergoalkeepertest.cpp
@@ -1,7 +1,35 @@
 #include "layer/main/fieldpositioncheckergoalkeepertest.h"
 #include "layer/main/fieldpositioncheckergoalkeeper.h"
+#include "common/geometry/point.h"
 
 using namespace RoboSoccer::Layer::Main;
+using namespace RoboSoccer::Common::Geometry;
 
+void FieldPositionCheckerGoalkeeperTest::isPointInsideField_pointInNeutralZone_true()
+{
+	FieldPositionCheckerGoalkeeper fieldPositionChecker;
 
+	CPPUNIT_ASSERT(fieldPositionChecker.isPointInsideField(Point(1,-0.6)));
+}
 
+void FieldPositionCheckerGoalkeeperTest::isPointInsideField_pointOutsideField_false()
+{
+	FieldPositionCheckerGoalkeeper fieldPositionChecker;
+
+	CPPUNIT_ASSERT(!fieldPositionChecker.isPointInsideField(Point(-0.3,2)));
+}
+
+void FieldPositionCheckerGoalkeeperTest::isPointInsideField_pointInOwnGoalZone_true()
+{
+	FieldPositionCheckerGoalkeeper fieldPositionChecker;
+
+	CPPUNIT_ASSERT(fieldPositionChecker.isPointInsideField(Point(1.3,-0.2)));
+}
+
+void FieldPositionCheckerGoalkeeperTest::isPointInsideField_pointInEnemyGoalZone_false()
+{
+	FieldPositionCheckerGoalkeeper fieldPositionChecker;
+
+	CPPUNIT_ASSERT(!fieldPositionChecker.isPointInsideField(Point(-1.3,0.2)));
+
+}
--- source/layer/main/fieldpositioncheckergoalkeepertest.h
+++ source/layer/main/fieldpositioncheckergoalkeepertest.h
@@ -14,8 +14,17 @@ namespace Main
 			public CPPUNIT_NS::TestFixture
 	{
 		CPPUNIT_TEST_SUITE(FieldPositionCheckerGoalkeeperTest);
+		CPPUNIT_TEST(isPointInsideField_pointInNeutralZone_true);
+		CPPUNIT_TEST(isPointInsideField_pointOutsideField_false);
+		CPPUNIT_TEST(isPointInsideField_pointInOwnGoalZone_true);
+		CPPUNIT_TEST(isPointInsideField_pointInEnemyGoalZone_false);
 		CPPUNIT_TEST_SUITE_END();
 	private:
+
+		void isPointInsideField_pointInNeutralZone_true();
+		void isPointInsideField_pointOutsideField_false();
+		void isPointInsideField_pointInOwnGoalZone_true();
+		void isPointInsideField_pointInEnemyGoalZone_false();
 
 	};
 }
