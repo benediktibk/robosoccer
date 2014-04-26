428a5b0ed3b4b1ca8f6ec9c9a2aa36f53272ba13
Florian Wirnshofer <florian.wirnshofer@hotmail.de>
2014-04-26 22:58:14 +0200

    Finished Implementation of Intelligen ball

--- source/layer/main/intelligentball.cpp
+++ source/layer/main/intelligentball.cpp
@@ -1,10 +1,44 @@
 #include "layer/main/intelligentball.h"
 #include "layer/abstraction/ball.h"
+#include "common/geometry/orientedposition.h"
+#include "common/geometry/circle.h"
 
 using namespace RoboSoccer::Layer::Main;
 using namespace RoboSoccer::Layer::Abstraction;
+using namespace RoboSoccer::Common;
 
 IntelligentBall::IntelligentBall()
 {
 
+}
+
+Geometry::OrientedPosition IntelligentBall::getPosition() const
+{
+	return m_ball->getPosition();
+}
+
+Geometry::Circle IntelligentBall::getObstacle() const
+{
+	return m_ball->getObstacle();
+}
+
+double IntelligentBall::getVelocity() const
+{
+	return m_ball->getVelocity();
+}
+
+bool IntelligentBall::isMoving(double velocity) const
+{
+	return(velocity > 0.1);
+	//TODO: find appropriate Threshold
+}
+
+FieldSide IntelligentBall::getMovingDirection(Geometry::OrientedPosition position) const
+{
+	Geometry::Angle direction = position.getOrientation();
+	double angle=direction.getValueBetweenMinusPiAndPi();
+	if (angle > 0)
+		return FieldSideRight;
+	else
+		return FieldSideLeft;
 }
--- source/layer/main/intelligentball.h
+++ source/layer/main/intelligentball.h
@@ -1,20 +1,37 @@
 #ifndef ROBOSOCCER_LAYER_MAIN_INTELLIGENTBALL_H
 #define ROBOSOCCER_LAYER_MAIN_INTELLIGENTBALL_H
 
-class Ball;
+#include "layer/abstraction/fieldside.h"
 
 namespace RoboSoccer
 {
+namespace Common
+{
+namespace Geometry
+{
+	class OrientedPosition;
+	class Circle;
+}
+}
 namespace Layer
 {
+namespace Abstraction
+{
+	class Ball;
+}
 namespace Main
 {
 	class IntelligentBall
 	{
 	public:
 		IntelligentBall();
+		Common::Geometry::OrientedPosition getPosition() const;
+		Common::Geometry::Circle getObstacle() const;
+		double getVelocity() const;
+		bool isMoving(double velocity) const;
+		Abstraction::FieldSide getMovingDirection(Common::Geometry::OrientedPosition position) const;
 	private:
-		Ball *m_ball;
+		Abstraction::Ball *m_ball;
 	};
 }
 }
