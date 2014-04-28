fac19be30d5bbc8fc214406cceea9b920939e809
Benedikt Schmidt <benediktibk@aon.at>
2014-04-28 12:23:32 +0200

    added logging of states to abstraction test

--- source/main/mainabstractiontest.cpp
+++ source/main/mainabstractiontest.cpp
@@ -3,16 +3,22 @@
 #include "layer/abstraction/controllablerobotimpl.h"
 #include "layer/abstraction/storageimpl.h"
 #include "layer/abstraction/refereebase.h"
+#include "layer/control/pause.h"
 #include "common/geometry/point.h"
 #include "common/geometry/orientedposition.h"
 #include "common/logging/loggerimpl.h"
+#include "common/states/statemachine.h"
+#include "common/other/console.h"
 #include <iostream>
 #include <unistd.h>
 #include <stdio.h>
 
 using namespace RoboSoccer::Layer::Abstraction;
+using namespace RoboSoccer::Layer::Control;
 using namespace RoboSoccer::Common::Geometry;
 using namespace RoboSoccer::Common::Logging;
+using namespace RoboSoccer::Common::States;
+using namespace RoboSoccer::Common::Other;
 using namespace std;
 
 int main(int, char**)
@@ -29,16 +35,19 @@ int main(int, char**)
 	cout << "current ball orientation is " << ball.getRotation() << endl;
 	cout << "current position of enemy robot is " << enemyRobot.getPosition() << endl;
 
-	referee.logInformation();
-	referee.setReady();
-	referee.logInformation();
-
 	cout << "moving own robot to position (0, 0) after next keypress" << endl;
 	getchar();
 	ownRobot.gotoPositionPrecise(Point());
 
 	cout << "waiting a little bit" << endl;
 	sleep(5);
+
+	Pause *initialState = new Pause(logger, referee);
+	StateMachine stateMachine(initialState);
+
+	cout << "exit with q" << endl;
+	while(Console::getAsynchronousInput() != 'q')
+		stateMachine.update();
 
 	cout << "job should be done, closing the application" << endl;
 	return 0;
