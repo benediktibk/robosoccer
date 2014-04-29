14be13c6da94b8a3ba6c41b5ebef64233ed33beb
Markus Hofbauer <markus.hofbauer@tum.de>
2014-04-29 15:05:58 +0200

    add robot.h

---
+++ source/layer/autonomous/robot.h
@@ -0,0 +1,22 @@
+#ifndef ROBOSOCCER_LAYER_AUTONOMOUS_ROBOT_H
+#define ROBOSOCCER_LAYER_AUTONOMOUS_ROBOT_H
+
+namespace RoboSoccer
+{
+namespace Layer
+{
+namespace Autonomous
+{
+	class Robot
+	{
+	public:
+		virtual ~Robot() { }
+
+	private:
+
+	};
+}
+}
+}
+
+#endif
