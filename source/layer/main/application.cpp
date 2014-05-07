7b309fc1ed970b040e3bec5cb81ad94bb053f106
Benedikt Schmidt <benediktibk@aon.at>
2014-05-07 14:47:11 +0200

    added call to measure of the robots

--- source/layer/main/application.cpp
+++ source/layer/main/application.cpp
@@ -63,6 +63,12 @@ void Application::run()
 
 	while (!stop)
 	{
+		for (unsigned int i = 0; i < 3; ++i)
+		{
+			Robot &robot = m_ownTeam->getRobotByNumber(i);
+			robot.measure();
+		}
+
 		FieldSide ownSide = referee.getOwnFieldSide();
 		m_targetPositionFetcher->setFieldSide(ownSide);
 		string previousState = stateMachine.getNameOfCurrentState();
