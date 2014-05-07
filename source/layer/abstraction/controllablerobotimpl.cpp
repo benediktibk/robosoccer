997d22d542360b074b57a31d66753c8559d6e95b
Florian Wirnshofer <florian.wirnshofer@hotmail.de>
2014-05-07 18:45:18 +0200

    minor changes

--- source/layer/abstraction/controllablerobotimpl.cpp
+++ source/layer/abstraction/controllablerobotimpl.cpp
@@ -19,7 +19,7 @@ ControllableRobotImpl::ControllableRobotImpl(
 		unsigned int deviceId, KogniMobil::RTDBConn &dataBase, TeamColor color, Watch const &watch) :
 	m_turnControl(new RobotTurnControl(watch)),
 	m_driveShortControl(new RobotDriveControl(watch, 0.2, 0.2, 50, 40)),
-	m_driveLongControl(new RobotDriveControl(watch, 0.1, 0.09, 50, 40)),
+	m_driveLongControl(new RobotDriveControl(watch, 0.1, 0.05, 50, 40)),
 	m_translationSpeed(0),
 	m_rotationSpeed(0),
 	m_loopTimeWatch(new StopWatch(watch))
@@ -130,7 +130,7 @@ void ControllableRobotImpl::update()
 		break;
 	case StateDrivingLong:
 		if(getPosition().distanceTo(m_driveTarget) > 0.1)
-			m_driveShortControl->evaluate(getPose(), m_driveTarget, translationSpeed, rotationSpeed);
+			m_driveLongControl->evaluate(getPose(), m_driveTarget, translationSpeed, rotationSpeed);
 		else
 			switchInto(StateStop);
 		break;
