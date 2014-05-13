6d50928eb797951ea75aadb9f5c88b8e9f7b3fbf
Benedikt Schmidt <benediktibk@aon.at>
2014-05-13 18:37:26 +0200

    don't allow multiple kicks inside penaltyoffensive

--- source/layer/control/penaltyoffensive.cpp
+++ source/layer/control/penaltyoffensive.cpp
@@ -18,7 +18,8 @@ PenaltyOffensive::PenaltyOffensive(Logger &logger, RefereeBase &referee, Team &o
 		const EnemyTeam &enemyTeam, const Autonomous::IntelligentBall &ball,
 		Autonomous::TargetPositionFetcher const &targetPositionFetcher) :
 	RoboSoccerState(logger, referee, ownTeam, enemyTeam, ball, targetPositionFetcher, false),
-	m_calledGoTo(false)
+	m_calledGoTo(false),
+	m_ballKicked(false)
 { }
 
 State *PenaltyOffensive::nextState()
@@ -46,8 +47,9 @@ void PenaltyOffensive::updateInternal()
 		m_calledGoTo = true;
 	}
 
-	if(movementsFinished())
+	if(movementsFinished() && !m_ballKicked)
 	{
 		robot.kick(100, m_ball);
+		m_ballKicked = true;
 	}
 }
