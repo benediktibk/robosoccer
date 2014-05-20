fd1708eb258160d13d4cddbe047539e952345f32
Kevin Meyer <meyer.kevin@mytum.de>
2014-05-20 15:43:13 +0200

    added goTo calls

--- source/layer/control/treenoderesultdefendgoal.cpp
+++ source/layer/control/treenoderesultdefendgoal.cpp
@@ -1,7 +1,10 @@
 #include "layer/control/treenoderesultdefendgoal.h"
+#include "layer/autonomous/team.h"
+#include "layer/autonomous/robot.h"
+#include "common/geometry/pose.h"
 
+using namespace RoboSoccer::Common::Geometry;
 using namespace RoboSoccer::Layer::Control;
-
 
 TreeNodeResultDefendGoal::TreeNodeResultDefendGoal(
 		RoboSoccer::Common::Logging::Logger &logger, RoboSoccer::Layer::Abstraction::RefereeBase &referee,
@@ -14,5 +17,7 @@ TreeNodeResultDefendGoal::TreeNodeResultDefendGoal(
 
 void TreeNodeResultDefendGoal::execute()
 {
-
+	//! @todo use useful targets
+	m_ownTeam.getFirstFieldPlayer().goTo(Pose(Point(-0.5,-0.5), Angle()));
+	m_ownTeam.getSecondFieldPlayer().goTo(Pose(Point(-0.5,0.5), Angle()));
 }
