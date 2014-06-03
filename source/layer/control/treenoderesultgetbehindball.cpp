4f73c36492452900649e3e4f909eb7e6a1c2dab0
Kevin Meyer <meyer.kevin@mytum.de>
2014-06-03 11:59:51 +0200

    basic logic for GetBehindBall

--- source/layer/control/treenoderesultgetbehindball.cpp
+++ source/layer/control/treenoderesultgetbehindball.cpp
@@ -2,7 +2,10 @@
 #include "layer/autonomous/team.h"
 #include "layer/autonomous/robot.h"
 #include "common/geometry/pose.h"
+#include "layer/autonomous/intelligentball.h"
+#include "layer/autonomous/targetpositionfetcher.h"
 
+using namespace RoboSoccer::Layer::Autonomous;
 using namespace RoboSoccer::Common::Geometry;
 using namespace RoboSoccer::Layer::Control;
 
@@ -17,8 +20,21 @@ TreeNodeResultGetBehindBall::TreeNodeResultGetBehindBall(
 
 void TreeNodeResultGetBehindBall::execute()
 {
-	//! @todo use useful targets
-	//! @todo consider ignoreBall and driveSlowlyAtTheEnd
-	m_ownTeam.getFirstFieldPlayer().goTo(Pose(Point(-0.5,-0.5), Angle()), false, false);
-	m_ownTeam.getSecondFieldPlayer().goTo(Pose(Point(-0.5,0.5), Angle()), false, false);
+	//! @todo use useful target for second robot
+	Robot &robot1 = m_ownTeam.getFirstFieldPlayer();
+	Robot &robot2 = m_ownTeam.getSecondFieldPlayer();
+
+	Point target;
+	if (m_ball.isMoving())
+		target = m_targetPositionFetcher.getPointBehindBallInMovingDirection(m_ball, 0.3);
+
+	else
+		target = m_targetPositionFetcher.getPointBehindBallInMovingDirection(m_ball, 0.1);
+
+	if (robot1.getCurrentPose().getPosition().distanceTo(target) <
+			robot2.getCurrentPose().getPosition().distanceTo(target))
+	{
+		robot1.goTo(Pose(target, Angle()), false, false);
+	}
+
 }
