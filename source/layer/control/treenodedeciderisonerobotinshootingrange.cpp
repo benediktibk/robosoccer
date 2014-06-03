cf3e2826d393aa02436644607ce15f8d3f38e6d5
Kevin Meyer <meyer.kevin@mytum.de>
2014-06-03 11:32:53 +0200

    implemented first logic for IsOneRobotInShootingRange

--- source/layer/control/treenodedeciderisonerobotinshootingrange.cpp
+++ source/layer/control/treenodedeciderisonerobotinshootingrange.cpp
@@ -3,8 +3,17 @@
 #include "layer/control/treenoderesultshoot.h"
 #include "layer/autonomous/intelligentball.h"
 #include "layer/autonomous/targetpositionfetcher.h"
+#include "layer/autonomous/team.h"
+#include "layer/autonomous/robot.h"
+#include "layer/abstraction/refereebase.h"
+#include "common/geometry/pose.h"
+#include "common/geometry/rectangle.h"
+#include "common/other/compare.h"
 
 using namespace RoboSoccer::Layer::Control;
+using namespace RoboSoccer::Layer::Abstraction;
+using namespace RoboSoccer::Common::Geometry;
+using namespace RoboSoccer::Common::Other;
 
 TreeNodeDeciderIsOneRobotInShootingRange::TreeNodeDeciderIsOneRobotInShootingRange(
 		RoboSoccer::Common::Logging::Logger &logger, RoboSoccer::Layer::Abstraction::RefereeBase &referee,
@@ -15,5 +24,28 @@ TreeNodeDeciderIsOneRobotInShootingRange::TreeNodeDeciderIsOneRobotInShootingRan
 
 bool TreeNodeDeciderIsOneRobotInShootingRange::calculateDecision()
 {
-	return true;
+	Pose robot1Pose = m_ownTeam.getFirstFieldPlayer().getCurrentPose();
+	Pose robot2Pose = m_ownTeam.getSecondFieldPlayer().getCurrentPose();
+	Point ballPosition = m_ball.getPosition();
+
+	//! @todo find shooting range
+	if (ballPosition.distanceTo(robot1Pose.getPosition()) > 0.3
+			&& ballPosition.distanceTo(robot2Pose.getPosition()) > 0.3)
+		return false;
+
+	Point yCoordinate;
+
+	if (m_referee.getOwnFieldSide() == FieldSideRight)
+		yCoordinate = Point(0.2,0);
+	else
+		yCoordinate = Point(-0.2,0);
+
+	Point nearBall = ballPosition + Point(0,0.1);
+	Point farFromBall = ballPosition + Point(0,-0.1) + yCoordinate;
+	Rectangle shootingZone(nearBall, farFromBall);
+
+	Compare compare(0.01);
+
+	return shootingZone.isInside(robot1Pose.getPosition(), compare)
+			|| shootingZone.isInside(robot2Pose.getPosition(), compare);
 }
