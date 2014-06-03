879f00857346f916f7b97e9e889027f5ffaa8186
Kevin Meyer <meyer.kevin@mytum.de>
2014-06-03 12:23:48 +0200

    added executable maincontroltest

---
+++ source/main/maincontroltest.cpp
@@ -0,0 +1,62 @@
+#include "layer/abstraction/storageimpl.h"
+#include "layer/abstraction/refereemock.h"
+#include "layer/autonomous/teamimpl.h"
+#include "layer/autonomous/enemyteamimpl.h"
+#include "layer/autonomous/intelligentballimpl.h"
+#include "layer/autonomous/targetpositionfetcher.h"
+#include "layer/autonomous/obstaclefetcherimpl.h"
+#include "layer/main/fieldpositioncheckergoalkeeper.h"
+#include "layer/main/fieldpositioncheckerfieldplayer.h"
+#include "layer/autonomous/robot.h"
+#include "common/logging/loggerimpl.h"
+#include "common/time/watchimpl.h"
+#include "common/geometry/pose.h"
+#include "layer/control/treenodedeciderisonerobotinshootingrange.h"
+#include "layer/control/treenoderesult.h"
+#include <iostream>
+#include <unistd.h>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+using namespace RoboSoccer::Layer::Autonomous;
+using namespace RoboSoccer::Layer::Abstraction;
+using namespace RoboSoccer::Common::Geometry;
+using namespace RoboSoccer::Common::Logging;
+using namespace RoboSoccer::Common::Time;
+using namespace RoboSoccer::Common::Routing;
+using namespace RoboSoccer::Layer::Main;
+using namespace RoboSoccer::Layer::Control;
+
+int main(int, char**)
+{
+	cout << "creating objects" << endl;
+	LoggerImpl logger;
+	WatchImpl watch;
+	StorageImpl storage(14, TeamColorRed, logger, watch);
+	FieldPositionCheckerGoalkeeper fieldPositionCheckerGoalKeeper;
+	FieldPositionCheckerFieldPlayer fieldPositionCheckerFieldPlayer;
+	ObstacleFetcherImpl obstacleFetcher;
+	TeamImpl team(storage, watch, logger, fieldPositionCheckerGoalKeeper, fieldPositionCheckerFieldPlayer, obstacleFetcher);
+	EnemyTeamImpl enemyTeam(storage);
+	IntelligentBallImpl ball(storage.getBall());
+	TargetPositionFetcher targetPositionFetcher;
+	RefereeMock referee;
+	targetPositionFetcher.setFieldSide(referee.getOwnFieldSide());
+
+	while (true)
+	{
+		TreeNode *node = new TreeNodeDeciderIsOneRobotInShootingRange(logger, referee, team, enemyTeam, ball, targetPositionFetcher);
+		while (node->decide())
+		{
+			TreeNodeDecider *current = dynamic_cast<TreeNodeDecider*>(node);
+			node = current->getChild();
+			delete current;
+		}
+
+		TreeNodeResult *result = dynamic_cast<TreeNodeResult*>(node);
+		result->execute();
+
+		usleep(10000);
+	}
+}
