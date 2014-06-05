45940d6ec9573eee823f760e392f7cdb7424e0a5
Benedikt Schmidt <benediktibk@aon.at>
2014-06-05 11:54:18 +0200

    added test for drive to initial rotation

--- source/layer/autonomous/robotstates/drivetoinitialrotationtest.cpp
+++ source/layer/autonomous/robotstates/drivetoinitialrotationtest.cpp
@@ -5,6 +5,7 @@
 #include "layer/abstraction/controllablerobotmock.h"
 #include "common/logging/loggermock.h"
 #include "common/routing/routermock.h"
+#include "common/geometry/compare.h"
 
 using namespace RoboSoccer::Layer::Autonomous;
 using namespace RoboSoccer::Layer::Abstraction;
@@ -19,4 +20,23 @@ RobotState *DriveToInitialRotationTest::createInstance()
 				*m_controllableRobot, Pose(Point(5, 4), Angle::getQuarterRotation()),
 				*m_router, *m_logger, Logger::LogFileTypeAutonomousRobotGoalie, *m_obstacleFetcher,
 				*m_autonomousRobotMock, false, false, false);
+}
+
+void DriveToInitialRotationTest::update_onceCalled_robotGotOneCallToTurnToSecondPoint()
+{
+	m_robotState->update();
+
+	Compare compare(0.0001);
+	CPPUNIT_ASSERT_EQUAL((unsigned int)1, m_controllableRobot->getCallsToTurn());
+	CPPUNIT_ASSERT(compare.isFuzzyEqual(Angle(Point(0, 0), Point(5, 4)), m_controllableRobot->getLastAngleToTurnTo()));
+}
+
+void DriveToInitialRotationTest::update_twiceCalled_robotGotOneCallToTurnToSecondPoint()
+{
+	m_robotState->update();
+	m_robotState->update();
+
+	Compare compare(0.0001);
+	CPPUNIT_ASSERT_EQUAL((unsigned int)1, m_controllableRobot->getCallsToTurn());
+	CPPUNIT_ASSERT(compare.isFuzzyEqual(Angle(Point(0, 0), Point(5, 4)), m_controllableRobot->getLastAngleToTurnTo()));
 }
--- source/layer/autonomous/robotstates/drivetoinitialrotationtest.h
+++ source/layer/autonomous/robotstates/drivetoinitialrotationtest.h
@@ -13,13 +13,16 @@ namespace Autonomous
 			public RobotStateTest
 	{
 		CPPUNIT_TEST_SUITE(DriveToInitialRotationTest);
+		CPPUNIT_TEST(update_onceCalled_robotGotOneCallToTurnToSecondPoint);
+		CPPUNIT_TEST(update_twiceCalled_robotGotOneCallToTurnToSecondPoint);
 		CPPUNIT_TEST_SUITE_END();
 
 	protected:
 		virtual RobotState* createInstance();
 
 	private:
-
+		void update_onceCalled_robotGotOneCallToTurnToSecondPoint();
+		void update_twiceCalled_robotGotOneCallToTurnToSecondPoint();
 	};
 }
 }
