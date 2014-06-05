ce523877dfd07114f84cee2cf29818b3ad9074b8
Benedikt Schmidt <benediktibk@aon.at>
2014-06-05 12:05:38 +0200

    added tests for drive to initial rotation

--- source/layer/autonomous/robotstates/drivetoinitialrotationtest.cpp
+++ source/layer/autonomous/robotstates/drivetoinitialrotationtest.cpp
@@ -1,5 +1,7 @@
 #include "layer/autonomous/robotstates/drivetoinitialrotationtest.h"
 #include "layer/autonomous/robotstates/drivetoinitialrotation.h"
+#include "layer/autonomous/robotstates/drivetoinvalidroute.h"
+#include "layer/autonomous/robotstates/drivetodriving.h"
 #include "layer/autonomous/obstaclefetchermock.h"
 #include "layer/autonomous/robotmock.h"
 #include "layer/abstraction/controllablerobotmock.h"
@@ -39,4 +41,63 @@ void DriveToInitialRotationTest::update_twiceCalled_robotGotOneCallToTurnToSecon
 	Compare compare(0.0001);
 	CPPUNIT_ASSERT_EQUAL((unsigned int)1, m_controllableRobot->getCallsToTurn());
 	CPPUNIT_ASSERT(compare.isFuzzyEqual(Angle(Point(0, 0), Point(5, 4)), m_controllableRobot->getLastAngleToTurnTo()));
+}
+
+void DriveToInitialRotationTest::nextState_routeBecomesInvalidAndNoNewOnePossible_invalidRoute()
+{
+	vector<Circle> obstacles;
+	obstacles.push_back(Circle(Point(3, 3), 1));
+	m_obstacleFetcher->setAllObstaclesButMeInRange(obstacles);
+	m_router->setInvalidRoute();
+
+	RobotState *nextState = m_robotState->nextState(false);
+
+	DriveToInvalidRoute *nextStateCasted = dynamic_cast<DriveToInvalidRoute*>(nextState);
+	CPPUNIT_ASSERT(nextStateCasted != 0);
+	delete nextState;
+}
+
+void DriveToInitialRotationTest::nextState_routeBecomesInvalidAndNewOnePossible_initialRotation()
+{
+	vector<Circle> obstacles;
+	obstacles.push_back(Circle(Point(3, 3), 1));
+	m_obstacleFetcher->setAllObstaclesButMeInRange(obstacles);
+
+	RobotState *nextState = m_robotState->nextState(false);
+
+	DriveToInitialRotation *nextStateCasted = dynamic_cast<DriveToInitialRotation*>(nextState);
+	CPPUNIT_ASSERT(nextStateCasted != 0);
+	delete nextState;
+
+}
+
+void DriveToInitialRotationTest::nextState_targetNotReached_0()
+{
+	m_controllableRobot->setPose(Pose(Point(0, 0), Angle::getHalfRotation()));
+
+	RobotState *nextState = m_robotState->nextState(false);
+
+	CPPUNIT_ASSERT(nextState == 0);
+}
+
+void DriveToInitialRotationTest::nextState_movementStopped_driving()
+{
+	m_controllableRobot->setPose(Pose(Point(0, 0), Angle::getHalfRotation()));
+
+	RobotState *nextState = m_robotState->nextState(true);
+
+	DriveToDriving *nextStateCasted = dynamic_cast<DriveToDriving*>(nextState);
+	CPPUNIT_ASSERT(nextStateCasted != 0);
+	delete nextState;
+}
+
+void DriveToInitialRotationTest::nextState_targetReached_driving()
+{
+	m_controllableRobot->setPose(Pose(Point(0, 0), Angle(Point(0, 0), Point(5, 4))));
+
+	RobotState *nextState = m_robotState->nextState(false);
+
+	DriveToDriving *nextStateCasted = dynamic_cast<DriveToDriving*>(nextState);
+	CPPUNIT_ASSERT(nextStateCasted != 0);
+	delete nextState;
 }
--- source/layer/autonomous/robotstates/drivetoinitialrotationtest.h
+++ source/layer/autonomous/robotstates/drivetoinitialrotationtest.h
@@ -15,6 +15,11 @@ namespace Autonomous
 		CPPUNIT_TEST_SUITE(DriveToInitialRotationTest);
 		CPPUNIT_TEST(update_onceCalled_robotGotOneCallToTurnToSecondPoint);
 		CPPUNIT_TEST(update_twiceCalled_robotGotOneCallToTurnToSecondPoint);
+		CPPUNIT_TEST(nextState_routeBecomesInvalidAndNoNewOnePossible_invalidRoute);
+		CPPUNIT_TEST(nextState_routeBecomesInvalidAndNewOnePossible_initialRotation);
+		CPPUNIT_TEST(nextState_targetNotReached_0);
+		CPPUNIT_TEST(nextState_movementStopped_driving);
+		CPPUNIT_TEST(nextState_targetReached_driving);
 		CPPUNIT_TEST_SUITE_END();
 
 	protected:
@@ -23,6 +28,11 @@ namespace Autonomous
 	private:
 		void update_onceCalled_robotGotOneCallToTurnToSecondPoint();
 		void update_twiceCalled_robotGotOneCallToTurnToSecondPoint();
+		void nextState_routeBecomesInvalidAndNoNewOnePossible_invalidRoute();
+		void nextState_routeBecomesInvalidAndNewOnePossible_initialRotation();
+		void nextState_targetNotReached_0();
+		void nextState_movementStopped_driving();
+		void nextState_targetReached_driving();
 	};
 }
 }
