3ab1aac7a866abb6e1e4e7b71cd322ccfadd4aed
Benedikt Schmidt <benediktibk@aon.at>
2014-06-05 15:12:33 +0200

    added tests for drive to direct initial rotation

--- source/layer/autonomous/robotstates/drivetodirectinitialrotationtest.cpp
+++ source/layer/autonomous/robotstates/drivetodirectinitialrotationtest.cpp
@@ -1,5 +1,6 @@
 #include "layer/autonomous/robotstates/drivetodirectinitialrotationtest.h"
 #include "layer/autonomous/robotstates/drivetodirectinitialrotation.h"
+#include "layer/autonomous/robotstates/drivetodirectdriving.h"
 #include "layer/abstraction/controllablerobotmock.h"
 #include "common/logging/loggermock.h"
 #include "common/geometry/compare.h"
@@ -37,4 +38,36 @@ void DriveToDirectInitialRotationTest::update_twiceCalled_robotGotCallToTurnToTa
 	Compare compare(0.0001);
 	CPPUNIT_ASSERT_EQUAL((unsigned int)1, m_controllableRobot->getCallsToTurn());
 	CPPUNIT_ASSERT(compare.isFuzzyEqual(Angle(Point(0, 0), Point(5, 4)), m_controllableRobot->getLastAngleToTurnTo()));
+}
+
+void DriveToDirectInitialRotationTest::nextState_rotationNotReached_0()
+{
+	m_controllableRobot->setPose(Pose(Point(0, 0), Angle::getHalfRotation()));
+
+	RobotState *nextState = m_robotState->nextState(false);
+
+	CPPUNIT_ASSERT(nextState == 0);
+}
+
+void DriveToDirectInitialRotationTest::nextState_rotationReached_driving()
+{
+	m_controllableRobot->setPose(Pose(Point(0, 0), Angle(Point(0, 0), Point(5, 4))));
+
+	RobotState *nextState = m_robotState->nextState(false);
+
+	DriveToDirectDriving *nextStateCasted = dynamic_cast<DriveToDirectDriving*>(nextState);
+	CPPUNIT_ASSERT(nextStateCasted != 0);
+	delete nextState;
+}
+
+void DriveToDirectInitialRotationTest::nextState_movementStopped_driving()
+{
+	m_controllableRobot->setPose(Pose(Point(0, 0), Angle::getHalfRotation()));
+	m_robotState->update();
+
+	RobotState *nextState = m_robotState->nextState(true);
+
+	DriveToDirectDriving *nextStateCasted = dynamic_cast<DriveToDirectDriving*>(nextState);
+	CPPUNIT_ASSERT(nextStateCasted != 0);
+	delete nextState;
 }
--- source/layer/autonomous/robotstates/drivetodirectinitialrotationtest.h
+++ source/layer/autonomous/robotstates/drivetodirectinitialrotationtest.h
@@ -16,6 +16,9 @@ namespace Autonomous
 		CPPUNIT_TEST(reachedTarget_empty_false);
 		CPPUNIT_TEST(update_onceCalled_robotGotCallToTurnToTarget);
 		CPPUNIT_TEST(update_twiceCalled_robotGotCallToTurnToTarget);
+		CPPUNIT_TEST(nextState_rotationNotReached_0);
+		CPPUNIT_TEST(nextState_rotationReached_driving);
+		CPPUNIT_TEST(nextState_movementStopped_driving);
 		CPPUNIT_TEST_SUITE_END();
 
 	protected:
@@ -25,6 +28,9 @@ namespace Autonomous
 		void reachedTarget_empty_false();
 		void update_onceCalled_robotGotCallToTurnToTarget();
 		void update_twiceCalled_robotGotCallToTurnToTarget();
+		void nextState_rotationNotReached_0();
+		void nextState_rotationReached_driving();
+		void nextState_movementStopped_driving();
 	};
 }
 }
