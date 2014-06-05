aca5197929cc0c6694e62b7b7aaa566b34af86d7
Benedikt Schmidt <benediktibk@aon.at>
2014-06-05 14:42:31 +0200

    added tests for drive to final rotation

--- source/layer/autonomous/robotstates/drivetofinalrotationtest.h
+++ source/layer/autonomous/robotstates/drivetofinalrotationtest.h
@@ -13,13 +13,18 @@ namespace Autonomous
 			public RobotStateTest
 	{
 		CPPUNIT_TEST_SUITE(DriveToFinalRotationTest);
+		CPPUNIT_TEST(constructor_routeWithTwoPoints_routeHasTwoPoints);
+		CPPUNIT_TEST(update_onceCalled_robotGotOneCallToTurnToFinalRotation);
+		CPPUNIT_TEST(update_twiceCalled_robotGotOneCallToTurnToFinalRotation);
 		CPPUNIT_TEST_SUITE_END();
 
 	protected:
 		virtual RobotState* createInstance();
 
 	private:
-
+		void constructor_routeWithTwoPoints_routeHasTwoPoints();
+		void update_onceCalled_robotGotOneCallToTurnToFinalRotation();
+		void update_twiceCalled_robotGotOneCallToTurnToFinalRotation();
 	};
 }
 }
