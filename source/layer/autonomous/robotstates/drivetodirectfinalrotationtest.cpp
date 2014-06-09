38b1bdea03bb9e026ae29d54eac8449a6293700a
Benedikt Schmidt <benediktibk@aon.at>
2014-06-09 10:29:24 +0200

    added tests for drive to direct final rotation

--- source/layer/autonomous/robotstates/drivetodirectfinalrotationtest.cpp
+++ source/layer/autonomous/robotstates/drivetodirectfinalrotationtest.cpp
@@ -1,5 +1,6 @@
 #include "layer/autonomous/robotstates/drivetodirectfinalrotationtest.h"
 #include "layer/autonomous/robotstates/drivetodirectfinalrotation.h"
+#include "layer/autonomous/robotstates/reachedtarget.h"
 #include "layer/abstraction/controllablerobotmock.h"
 #include "common/logging/loggermock.h"
 #include "common/geometry/compare.h"
@@ -37,4 +38,34 @@ void DriveToDirectFinalRotationTest::update_twiceCalled_robotGotOneCallToTurnToF
 	Compare compare(0.00001);
 	CPPUNIT_ASSERT_EQUAL((unsigned int)1, m_controllableRobot->getCallsToTurn());
 	CPPUNIT_ASSERT(compare.isFuzzyEqual(Angle::getQuarterRotation(), m_controllableRobot->getLastAngleToTurnTo()));
+}
+
+void DriveToDirectFinalRotationTest::nextState_targetNotReached_0()
+{
+	RobotState *nextState = m_robotState->nextState(false);
+
+	CPPUNIT_ASSERT(nextState == 0);
+}
+
+void DriveToDirectFinalRotationTest::nextState_targetReached_reachedTarget()
+{
+	m_controllableRobot->setPose(Pose(Point(5, 4), Angle::getQuarterRotation()));
+
+	RobotState *nextState = m_robotState->nextState(false);
+
+	ReachedTarget *nextStateCasted = dynamic_cast<ReachedTarget*>(nextState);
+	CPPUNIT_ASSERT(nextStateCasted != 0);
+	delete nextState;
+}
+
+void DriveToDirectFinalRotationTest::nextState_movementStopped_reachedTarget()
+{
+	m_controllableRobot->setPose(Pose(Point(5, 4), Angle::getHalfRotation()));
+	m_robotState->update();
+
+	RobotState *nextState = m_robotState->nextState(true);
+
+	ReachedTarget *nextStateCasted = dynamic_cast<ReachedTarget*>(nextState);
+	CPPUNIT_ASSERT(nextStateCasted != 0);
+	delete nextState;
 }
--- source/layer/autonomous/robotstates/drivetodirectfinalrotationtest.h
+++ source/layer/autonomous/robotstates/drivetodirectfinalrotationtest.h
@@ -16,6 +16,9 @@ namespace Autonomous
 		CPPUNIT_TEST(reachedTarget_empty_false);
 		CPPUNIT_TEST(update_onceCalled_robotGotOneCallToTurnToFinalOrientation);
 		CPPUNIT_TEST(update_twiceCalled_robotGotOneCallToTurnToFinalOrientation);
+		CPPUNIT_TEST(nextState_targetNotReached_0);
+		CPPUNIT_TEST(nextState_targetReached_reachedTarget);
+		CPPUNIT_TEST(nextState_movementStopped_reachedTarget);
 		CPPUNIT_TEST_SUITE_END();
 
 	protected:
@@ -25,6 +28,9 @@ namespace Autonomous
 		void reachedTarget_empty_false();
 		void update_onceCalled_robotGotOneCallToTurnToFinalOrientation();
 		void update_twiceCalled_robotGotOneCallToTurnToFinalOrientation();
+		void nextState_targetNotReached_0();
+		void nextState_targetReached_reachedTarget();
+		void nextState_movementStopped_reachedTarget();
 	};
 }
 }
