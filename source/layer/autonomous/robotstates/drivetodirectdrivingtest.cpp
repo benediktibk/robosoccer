8a8097def8a1a8557f92ccecae02a0076666dbc6
Benedikt Schmidt <benediktibk@aon.at>
2014-06-09 10:32:01 +0200

    added tests for drive to direct driving

--- source/layer/autonomous/robotstates/drivetodirectdrivingtest.cpp
+++ source/layer/autonomous/robotstates/drivetodirectdrivingtest.cpp
@@ -2,6 +2,7 @@
 #include "layer/autonomous/robotstates/drivetodirectdriving.h"
 #include "layer/abstraction/controllablerobotmock.h"
 #include "common/logging/loggermock.h"
+#include "common/geometry/compare.h"
 
 using namespace RoboSoccer::Layer::Autonomous;
 using namespace RoboSoccer::Common::Geometry;
@@ -17,4 +18,23 @@ RobotState *DriveToDirectDrivingTest::createInstance()
 void DriveToDirectDrivingTest::reachedTarget_empty_false()
 {
 	CPPUNIT_ASSERT(!m_robotState->reachedTarget());
+}
+
+void DriveToDirectDrivingTest::update_onceCalled_robotGotOneCallToDriveToTarget()
+{
+	m_robotState->update();
+
+	Compare compare(0.00001);
+	CPPUNIT_ASSERT_EQUAL((unsigned int)1, m_controllableRobot->getCallsToGoToCombined());
+	CPPUNIT_ASSERT(compare.isFuzzyEqual(Point(5, 4), m_controllableRobot->getLastPointToDriveTo()));
+}
+
+void DriveToDirectDrivingTest::update_twiceCalled_robotGotOneCallToDriveToTarget()
+{
+	m_robotState->update();
+	m_robotState->update();
+
+	Compare compare(0.00001);
+	CPPUNIT_ASSERT_EQUAL((unsigned int)1, m_controllableRobot->getCallsToGoToCombined());
+	CPPUNIT_ASSERT(compare.isFuzzyEqual(Point(5, 4), m_controllableRobot->getLastPointToDriveTo()));
 }
