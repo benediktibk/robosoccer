91989592089ef8c1c241693c0e57a2c48f9e1f87
Kevin Meyer <meyer.kevin@mytum.de>
2014-06-10 11:04:38 +0200

    added Straight class

---
+++ source/common/geometry/straighttest.cpp
@@ -0,0 +1,7 @@
+#include "common/geometry/straighttest.h"
+#include "common/geometry/straight.h"
+
+using namespace RoboSoccer::Common::Geometry;
+
+
+
---
+++ source/common/geometry/straighttest.h
@@ -0,0 +1,27 @@
+#ifndef ROBOSOCCER_COMMON_GEOMETRY_STRAIGHTTEST_H
+#define ROBOSOCCER_COMMON_GEOMETRY_STRAIGHTTEST_H
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+namespace RoboSoccer
+{
+namespace Common
+{
+namespace Geometry
+{
+class StraightTest :
+		public CPPUNIT_NS::TestFixture
+	{
+		CPPUNIT_TEST_SUITE(StraightTest);
+		CPPUNIT_TEST_SUITE_END();
+	private:
+
+	};
+}
+}
+}
+
+#endif
+
+
