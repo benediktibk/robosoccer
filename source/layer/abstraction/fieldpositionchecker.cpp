763c03f91f9f02917f2d06f18d85736793cebbd2
Benedikt Schmidt <benediktibk@aon.at>
2014-06-25 16:01:35 +0200

    shrinked goal zone a little bit

--- source/layer/abstraction/fieldpositionchecker.cpp
+++ source/layer/abstraction/fieldpositionchecker.cpp
@@ -9,14 +9,14 @@ using namespace RoboSoccer::Common::Other;
 bool FieldPositionChecker::isPointInsideLeftGoalZone(const Point &position) const
 {
 	Compare compare(0.01);
-	Rectangle goalZoneLeft(Point(-1.45, -0.35), Point(-1.2, 0.35));
+	Rectangle goalZoneLeft(Point(-1.45, -0.34), Point(-1.21, 0.34));
 	return goalZoneLeft.isInside(position, compare);
 }
 
 bool FieldPositionChecker::isPointInsideRightGoalZone(const Point &position) const
 {
 	Compare compare(0.01);
-	Rectangle goalZoneRight(Point(1.2, -0.35), Point(1.45, 0.35));
+	Rectangle goalZoneRight(Point(1.21, -0.34), Point(1.45, 0.34));
 	return goalZoneRight.isInside(position, compare);
 }
 
